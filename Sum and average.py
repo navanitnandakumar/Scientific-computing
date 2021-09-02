867eebad58345492c7bf4cf53896f1788424f1ad
Navanit Nandakumar <navanitnandakumar@gmail.com>
2021-09-02 20:38:44 +0530

    Create Sum and average.py

---
+++ Sum and average.py
@@ -0,0 +1,7 @@
+def sumnavg():
+    x = int(input("Enter a number : "))
+    y = int(input("Enter another number : "))
+    sum = int(x+y)
+    avg = int(sum/2)
+    print("Sum = ", sum, "Average = ", avg)
+sumnavg()
